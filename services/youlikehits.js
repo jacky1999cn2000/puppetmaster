d5c07314bc633517f8b4d16effc4552d049f15a5
Jacky Zhao <liang.zhao83@gmail.com>
2018-08-17 10:49:33 -0700

    relative stable version

--- services/youlikehits.js
+++ services/youlikehits.js
@@ -25,50 +25,87 @@ module.exports = {
     await page.goto(config.youlikehits_twitter_followers);
     await page.waitFor(500);
 
-    const ids = await page.evaluate(
+    const followpIds = await page.evaluate(
+      () => [...document.querySelectorAll('.followp')]
+      .map(element => element.getAttribute('id'))
+    );
+
+    const followIds = await page.evaluate(
       () => [...document.querySelectorAll('.follow')]
       .map(element => element.getAttribute('id'))
     );
 
+    const ids = followpIds.concat(followIds);
+
+    console.log('ids ',ids.length);
+
     for (let i = 0; i < ids.length; i++) {
+
+      console.log('i ',i);
+      console.log('id ', ids[i]);
 
       let followButton = '#' + ids[i] + ' > center > center > a:nth-child(1)';
       let confirmButton = '#' + ids[i] + ' > center > center > a:nth-child(2)';
 
-      let twitterpage;
-      let skip = 'no';
+      let twitterpage = null;
+      let decision = 'RESET';
+      let state = 'RESET';
 
       await page.click(followButton);
 
       browser.on('targetcreated', async target => {
+
         if (target.url() !== 'about:blank') {
-          logger.log(`following: ${target.url()}`, 1);
+
+          await logger.log(`following: ${target.url()}`, 1);
+
           try {
             twitterpage = await target.page();
             await twitterpage.waitFor("#follow_btn_form > button", {
               timeout: 1000
             });
+            // await page.waitFor(4000);
             await twitterpage.click('#follow_btn_form > button');
-            await twitterpage.waitFor(500);
+            // await twitterpage.waitFor('form.unfollow > button',{
+            //   timeout: 1000
+            // });
+            await twitterpage.waitFor(1000);
+            decision = 'CONFIRM';
+            state = 'CONTINUE';
             await twitterpage.close();
           } catch (e) {
-            console.log('error ', e);
+            // console.log('error ', e);
+            console.log('error');
+            console.log('\rname', e.name);
+            console.log('\rmessage',e.message);
+            if(e.message.indexOf('of null') != -1){
+              state = 'EXIT';
+            }
+            decision = 'SKIP';
             await twitterpage.close();
-            skip = 'yes';
           }
+
         }
+
       });
-      console.log('skip ', skip);
+
       await page.waitFor(2000);
-      if (skip == 'no') {
-        console.log('confirm');
+
+      if (decision == 'CONFIRM') {
+        console.log(decision);
+        console.log(state);
         await page.click(confirmButton);
         await page.waitFor(6000);
       } else {
-        console.log('to skip');
+        console.log(decision);
+        console.log(state);
         let skipLink = '#' + ids[i] + ' > center > font > a';
         await page.click(skipLink);
-        await page.waitFor(1000);
+        await page.waitFor(4000);
+        if(state == 'EXIT'){
+          console.log('exit...');
+          process.exit(1);
+        }
       }
 
 

9039ee435bff6f87d12c86839e18dfb9264859fd
Jacky Zhao <liang.zhao83@gmail.com>
2018-08-19 00:30:36 -0700

    comment

--- modules/twitter.js
+++ modules/twitter.js
@@ -4,6 +4,7 @@ const youlikehits = require('../services/youlikehits');
 
 module.exports = async (page, browser, config) => {
 
+  // run follow automation for 5 loops (avoid rate limit), and break if points below 3
   for (let i = 0; i < 5; i++) {
     let status = await youlikehits.follow(page, browser, config);
     if (!status) {

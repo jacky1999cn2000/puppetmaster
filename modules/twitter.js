64b668002d14ea29fe91f0999f43186a886431a9
Jacky Zhao <liang.zhao83@gmail.com>
2018-08-19 00:21:46 -0700

    break the loop if points is below 3

--- modules/twitter.js
+++ modules/twitter.js
@@ -4,21 +4,13 @@ const youlikehits = require('../services/youlikehits');
 
 module.exports = async (page, browser, config) => {
 
-  // for (let i = 0; i < 5; i++) {
-  //   let status = await youlikehits.follow(page, browser, config);
-  //   console.log('status ', status);
-  //   if (status == 'break') {
-  //     break;
-  //   }
-  // }
-  //
-  let counter = 0;
-
-  while (counter < 30) {
-    counter += await youlikehits.follow(page, browser, config);
-    console.log('counter ', counter);
+  for (let i = 0; i < 5; i++) {
+    let status = await youlikehits.follow(page, browser, config);
+    if (!status) {
+      break;
+    }
   }
 
-  console.log('done');
+  console.log('outside');
 
 }
